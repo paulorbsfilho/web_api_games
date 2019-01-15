d4b5928f3d259d265b52edc3d38e73d24b94b3e8
paulorbsfilho <p4ulinho@outlook.com>
2019-01-15 14:39:42 -0300

    ADD validacao nome

--- games/serializers.py
+++ games/serializers.py
@@ -1,7 +1,17 @@
 from rest_framework import serializers
+from rest_framework.validators import UniqueTogetherValidator
+
 from .models import Game
 
 class GameSerializer(serializers.ModelSerializer):
     class Meta:
         model = Game
         fields = ('id', 'name', 'release_date', 'game_category')
+
+        validators = [
+            UniqueTogetherValidator(
+                queryset=Game.objects.all(),
+                fields=('name', 'game_category')
+            )
+        ]
+
